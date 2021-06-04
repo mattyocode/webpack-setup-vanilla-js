6c03cd2f9ed387f4c3ed9b651528cc1a4b5e1e96
mattyocode <matthewoliver@live.co.uk>
2021-06-04 17:13:39 +0100

    basic set up for webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+module.exports = {
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+  },
+  devtool: "cheap-module-eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
